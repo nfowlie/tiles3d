bb14b27abeb53ca4d5736cf4c94a0524d4c6f73d
Noel Echo Fowlie <noel.fowlie.dev@gmail.com>
2022-11-12 18:16:34 -0800

    Chrome Bug Fix

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
 const wrapper = document.getElementById('tiles')
+const targetWrapper = document.getElementById('targetTiles')
 
-const createTile = (index) => {
+const createTile = (index, isTarget) => {
 	const tile = document.createElement('div')
 	tile.classList.add('tile')
 	tile.style.opacity = 1
 	tile.dataset.index = index
 	tile.style.zIndex = index
 
-	const cubeSides = `
-	<div class="left"></div>
-	<div class="right"></div>
-	<div class="top"></div>
-	`
-	tile.innerHTML = cubeSides
+	if (isTarget) {
+		let animationTile = wrapper.querySelector('[data-index="' + index + '"]')
+		tile.addEventListener('pointerenter', () => {
+			animationTile.style.transform = 'translateX(200px) translateY(-200px)'
+			animationTile.children[0].style.backgroundColor = 'rgb(180, 180, 180)'
+			animationTile.children[1].style.backgroundColor = 'rgb(180, 180, 180)'
+			animationTile.children[2].style.backgroundColor = 'rgb(180, 180, 180)'
+		})
+		tile.addEventListener('pointerleave', () => {
+			animationTile.children[0].style.backgroundColor = 'rgb(20, 20, 20)'
+			animationTile.children[1].style.backgroundColor = 'rgb(20, 20, 20)'
+			animationTile.children[2].style.backgroundColor = 'rgb(20, 20, 20)'
+			animationTile.style.transform = 'translateX(0px) translateY(0px)'
+		})
+	} else {
+		const cubeSides = `
+			<div class="left"></div>
+			<div class="right"></div>
+			<div class="top"></div>
+		`
+		tile.innerHTML = cubeSides
 
-	tile.addEventListener('pointerenter', () => {
-		tile.style.transform = 'translateX(200px) translateY(-200px)'
-		tile.children[0].style.backgroundColor = 'rgb(180, 180, 180)'
-		tile.children[1].style.backgroundColor = 'rgb(180, 180, 180)'
-		tile.children[2].style.backgroundColor = 'rgb(180, 180, 180)'
-	})
-	tile.addEventListener('pointerleave', () => {
-		tile.children[0].style.backgroundColor = 'rgb(20, 20, 20)'
-		tile.children[1].style.backgroundColor = 'rgb(20, 20, 20)'
-		tile.children[2].style.backgroundColor = 'rgb(20, 20, 20)'
-		tile.style.transform = 'translateX(0px) translateY(0px)'
-	})
+
+	}
 
 	return tile
 }
 
 const createTiles = (quantity) => {
 	Array.from(Array(quantity)).map((tile, index) => {
-		wrapper.appendChild(createTile(index))
+		wrapper.appendChild(createTile(index, false))
+		targetWrapper.appendChild(createTile(index, true))
 	})
 }
 
 const createGrid = () => {
 	wrapper.innerHTML = ''
+	targetWrapper.innerHTML = ''
 	let rows = Math.floor(document.body.clientHeight / 50)
 	let columns = Math.floor(document.body.clientWidth / 50)
 
 	wrapper.style.setProperty('--rows', rows)
 	wrapper.style.setProperty('--columns', columns)
+	targetWrapper.style.setProperty('--rows', rows)
+	targetWrapper.style.setProperty('--columns', columns)
 
 	createTiles(rows * columns)
 }
